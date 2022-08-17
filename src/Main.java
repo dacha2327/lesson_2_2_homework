2970b929f569151ee807017e5060a713c703d6ac
dacha2327 <dachabas2327@gmail.com>
2022-08-17 22:53:52 +0600

    рц

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(createObject("BMW"));
-        System.out.println(createObject("Mazda"));
-        System.out.println(createObject("Toyota"));
-        System.out.println(createObject("ishebv"));
+        Printable[] cars = {createObject("Mazda"),createObject("Toyota"), createObject("BMW")};
+        for (Printable c1 :cars){
+            c1.print();
+        }
     }
 
     public static Car createObject(String className) {
         switch (className) {
             case "Mazda" -> {
                 Mazda mazda = new Mazda("rotor", 2003 , "kacb");
-                mazda.print();
                 return mazda;
             }
             case "Toyota" -> {
                 Toyota toyota = new Toyota("JZ", 1995, 3);
-                toyota.print();
                 return toyota;
             }
             case "BMW" -> {
                 BMW bmw = new BMW("V8", 2008, "rear");
-                bmw.print();
                 return bmw;
             }
         }
